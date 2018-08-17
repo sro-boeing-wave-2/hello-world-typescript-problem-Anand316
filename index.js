f5d12791bb53dbcf3eed848513548f72b9845f36
Anandkumar <anandkumar316sw@gmail.com>
2018-08-17 10:27:01 +0530

    Solution-1

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('sayHelloBtn').addEventListener('click', ClickFunction);
+function ClickFunction() {
+    document.getElementById('helloWorldBlock').innerHTML = "Hello World";
+}
+;
---
+++ index.ts
@@ -0,0 +1,4 @@
+document.getElementById('sayHelloBtn').addEventListener('click', ClickFunction);
+function ClickFunction(){
+    document.getElementById('helloWorldBlock').innerHTML = "Hello World";
+};
